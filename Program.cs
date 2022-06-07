ac3a07c6c979146e3db938f43d5bfff01c278552
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-07 15:17:28 -0300

    First insert with EntityFramework

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Blog
 {
@@ -6,7 +9,21 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            BlogDataContext.ConnectionString = config["Database:ConnectionString"];
+
+            using (var context = new BlogDataContext())
+            {
+                var tag = new Tag
+                {
+                    Name = "ASP.NET",
+                    Slug = "aspnet"
+                };
+
+                context.Tags.Add(tag);
+                context.SaveChanges();
+            }
         }
     }
 }
