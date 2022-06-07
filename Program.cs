608f92922e0e2d2f5d388889cdbef4ddf0e75219
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-07 17:10:32 -0300

    Starting tables mapping

--- Program.cs
+++ Program.cs
@@ -17,41 +17,6 @@ namespace Blog
 
             using (var context = new BlogDataContext())
             {
-                // var tag = new Tag
-                // {
-                //     Name = "ASP.NET",
-                //     Slug = "aspnet"
-                // };
-                // context.Tags.Add(tag);
-                // context.SaveChanges();
-
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-
-                // tag.Name = ".NET";
-                // tag.Slug = "dotnet";
-
-                // context.Update(tag);
-                // context.SaveChanges();
-
-
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-
-                // context.Remove(tag);
-                // context.SaveChanges();
-
-                // var tags = context.Tags.ToList();
-
-                // foreach (var tag in tags)
-                // {
-                //     Console.WriteLine($"Nome: {tag.Name}");
-                // }
-
-                var tag = context
-                    .Tags
-                    .AsNoTracking()
-                    .FirstOrDefault(x => x.Id == 3);
-
-                Console.WriteLine(tag.Name);
             }
         }
     }
