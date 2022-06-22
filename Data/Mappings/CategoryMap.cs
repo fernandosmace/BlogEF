a3d146050e4db8ee8d52e088952a0e18c4698556
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-22 16:43:34 -0300

    Add Fluent Mapping for Category

---
+++ Data/Mappings/CategoryMap.cs
@@ -0,0 +1,42 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //Table
+            builder.ToTable("Category");
+
+            //Primary Key
+            builder.HasKey(x => x.Id);
+
+            //Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            //Properties
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            //Index
+            builder.HasIndex(x => x.Slug, "IX_Category_Slug")
+                .IsUnique();
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
