9123ce84b52b65ff62fe8b9c7b2fec3af0eeeaf6
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-22 16:43:16 -0300

    Cleaning to use Fluent Mapping with EF Core

--- Program.cs
+++ Program.cs
@@ -11,65 +11,7 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 
-            BlogDataContext.ConnectionString = config["Database:ConnectionString"];
-
-            using var context = new BlogDataContext();
-
-            // var user = new User()
-            // {
-            //     Name = "André Baltieri",
-            //     Slug = "andrebaltieri",
-            //     Email = "andre@balta.io",
-            //     Bio = "9x Microsoft MVP",
-            //     Image = "http://",
-            //     PasswordHash = "123098457"
-            // };
-
-            // var category = new Category()
-            // {
-            //     Name = "Backend",
-            //     Slug = "backend"
-            // };
-
-            // var post = new Post()
-            // {
-            //     Title = "Meu primeiro post",
-            //     Author = user,
-            //     Category = category,
-            //     Body = "<p>Hello World!</p>",
-            //     Slug = "comecando-com-ef-core",
-            //     Summary = "Neste artigo vamos aprender EF Core",
-            //     CreateDate = DateTime.Now,
-            //     LastUpdateDate = DateTime.Now
-            // };
-
-            // context.Posts.Add(post);
-            // context.SaveChanges();
-
-            // var posts = context
-            //             .Posts
-            //             .AsNoTracking()
-            //             .Include(x => x.Author)
-            //             .Include(x => x.Category)
-            //             .OrderByDescending(x => x.LastUpdateDate)
-            //             .ToList();
-
-            // foreach (var post in posts)
-            //     Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
-
-            var post = context
-                        .Posts
-                        .Include(x => x.Author)
-                        .Include(x => x.Category)
-                        .OrderByDescending(x => x.LastUpdateDate)
-                        .FirstOrDefault();
-
-            post.Author.Name = "Teste";
-
-            context.Posts.Update(post);
-            context.SaveChanges();
         }
     }
 }
