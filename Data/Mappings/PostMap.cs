71e9a57658acc4c7eb0b232b275db7df4eaaa8c4
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-23 17:09:38 -0300

    Mapping Post with Fluent API

---
+++ Data/Mappings/PostMap.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("Post");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasColumnName("Title")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(160);
+
+            builder.Property(x => x.Summary)
+                .IsRequired()
+                .HasColumnName("Summary")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Body)
+                .IsRequired()
+                .HasColumnName("Body")
+                .HasColumnType("TEXT");
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("VARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.CreateDate)
+                .IsRequired()
+                .HasColumnName("CreateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.Category)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Category")
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post
+                        .HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_Post_Tag_PostId")
+                        .OnDelete(DeleteBehavior.NoAction),
+                    tag => tag
+                        .HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                );
+
+            builder.HasIndex(x => x.Slug, "IX_Post_Slug")
+                .IsUnique();
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
