237f701bc680fd1cf4358cf0db1b4cbb9408e64b
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-26 19:28:42 -0300

    Mapping UserRole relation

--- Data/Mappings/UserMap.cs
+++ Data/Mappings/UserMap.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -52,6 +53,24 @@ namespace Blog.Data.Mappings
 
             builder.HasIndex(x => x.Slug, "IX_User_Slug")
                 .IsUnique();
+
+            builder.HasMany(x => x.Roles)
+                .WithMany(x => x.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "UserRole",
+                    role => role
+                        .HasOne<Role>()
+                        .WithMany()
+                        .HasForeignKey("RoleId")
+                        .HasConstraintName("FK_UserRole_RoleId")
+                        .OnDelete(DeleteBehavior.NoAction),
+                    user => user
+                        .HasOne<User>()
+                        .WithMany()
+                        .HasForeignKey("UserId")
+                        .HasConstraintName("FK_UserRole_UserId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                );
         }
     }
 }
