3a956f596cb5b5ff1dd4d54808af4c33724998c9
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-26 22:43:38 -0300

    Testing the mapping relations

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Blog.Data;
+﻿using System;
+using System.Linq;
+using Blog.Data;
 using Blog.Models;
 using Microsoft.Extensions.Configuration;
 
@@ -10,6 +12,42 @@ namespace Blog
         {
             var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
             BlogDataContext.ConnectionString = config["Database:ConnectionString"];
+
+            using var context = new BlogDataContext();
+
+            // context.Users.Add(new User
+            // {
+            //     Bio = "9x Microsoft MVP",
+            //     Email = "balta@balta.io",
+            //     Image = "https://",
+            //     Name = "André Baltieri",
+            //     PasswordHash = "1234",
+            //     Slug = "andre-baltieri"
+            // });
+
+            // context.SaveChanges();
+
+            var user = context.Users.FirstOrDefault(u => u.Id == 6);
+
+            var post = new Post
+            {
+                Author = user,
+                Body = "Meu artigo",
+                Category = new Category
+                {
+                    Name = "BackEnd",
+                    Slug = "back-end"
+                },
+                CreateDate = DateTime.Now,
+                // LastUpdateDate =
+                Slug = "meu-artigo",
+                Summary = "Neste artigo vamos conferir...",
+                // Tags = null,
+                Title = "Meu artigo"
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
